5787c9c6b827b29737abae4a3205bcfc324fcf9a
cpolitano <cpolitano8@gmail.com>
2014-09-06 14:47:32 -0400

    initial views and css

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,15 +1,49 @@
 class QueriesController < ApplicationController
 
 	def index
-
-	end
-
-	def new
+		@queries = Query.all
 	end
 
 	def show
+		@query = Query.find(params[:id])
 	end
 
-	
+	def new
+		@query = Query.new
+	end
+
+  	def create
+  		@query = Query.new(query_params)
+  	if @query.save
+  	 	redirect_to @query
+  	else 
+  	  render 'new'
+  	end
+  end
+
+  def edit
+  	@query = Query.find(params[:id])
+  end
+
+  def update
+  	@query = Query.find(params[:id])
+  	if @query.update(query_params)
+  	redirect_to @query
+    else
+      render 'edit'
+  	end
+  end
+
+  def destroy
+  	@query = Query.find(params[:id])
+  	@query.destroy
+  	redirect_to queries_path
+  end
+
+  private
+
+  def query_params
+  	params.require(:query).permit(:keyword)
+  end
 
 end
--- app/models/query.rb
+++ app/models/query.rb
@@ -1,3 +1,4 @@
 class Query < ActiveRecord::Base
   belongs_to :user
+  validates :keyword, presence: true
 end
