882a39e8f74fac3afa991f005dc406fda3b9a4e2
cpolitano <cpolitano8@gmail.com>
2014-09-06 13:28:52 -0400

    start models and controllers

---
+++ app/controllers/queries_controller.rb
@@ -0,0 +1,15 @@
+class QueriesController < ApplicationController
+
+	def index
+
+	end
+
+	def new
+	end
+
+	def show
+	end
+
+	
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :queries
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'queries#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
